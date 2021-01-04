73a79f231f7161e1ecba559802937c598121f74e
Sunil Pai <sunil8627@gmail.com>
2021-01-04 22:58:31 +0530

    Adding scaffold to manage room types

---
+++ db/migrate/20210104172258_create_room_types.rb
@@ -0,0 +1,10 @@
+class CreateRoomTypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :room_types do |t|
+      t.string :name
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
